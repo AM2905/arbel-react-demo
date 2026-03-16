22c6d26a7f83c54816a7e0adf06b899b51d01b2d
AM2905 <arbel.mizrahi@gmail.com>
2026-03-17 00:15:26 +0200

    pls work6

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-import React from "react";
-import { useState } from "react";
-import "./css/App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import cat from "./assets/cat.png";
+import dog from "./assets/dog.png";
+import "./style.css";
+
+function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>press the cat!</h1>
+
+      <Link to="/second">
+        <img
+          src={cat}
+          alt="Go to second page"
+          style={{ cursor: "pointer" }}
+          className="catBtn"
+        />
+      </Link>
+    </div>
+  );
+}
+
+function SecondPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>press the dog</h1>
+      <Link to="/">
+      
+       <img
+          src={dog}
+          alt="Go to second page"
+          style={{ cursor: "pointer" }}
+          className="catBtn"
+        />
+      </Link>
+    </div>
+  );
+}
 
 function App() {
-
   return (
-    <>
-      <div className="app">
-      pls pls work!
-      <img src="./vite.svg" alt="vite" />
-      </div>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/second" element={<SecondPage />} />
+    </Routes>
   );
 }
 
